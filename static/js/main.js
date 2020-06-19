e84b71cef60efa04111a6ee4a4205ccfa31f703a
Prakhar <kriptyco@gmail.com>
2020-06-19 20:40:54 +0530

    Make Navbar Items Functional

--- static/js/main.js
+++ static/js/main.js
@@ -7,14 +7,18 @@ $(document).ready(function () {
 
 function checkitem() {
   var $this = $("#content");
+  $("nav ul").children().children().removeAttr("style");
   if ($(".carousel-inner .carousel-item:first").hasClass("active")) {
     $this.children("a.carousel-control-prev").hide();
     $this.children("a.carousel-control-next").show();
+    $("nav ul li:first a").css("color", "white");
   } else if ($(".carousel-inner .carousel-item:last").hasClass("active")) {
     $this.children("a.carousel-control-prev").show();
     $this.children("a.carousel-control-next").hide();
+    $("nav ul li:last a").css("color", "white");
   } else {
     $this.children("a.carousel-control-prev").show();
     $this.children("a.carousel-control-next").show();
+    $("nav ul li:nth-child(2) a").css("color", "white");
   }
 }
